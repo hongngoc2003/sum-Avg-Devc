bb615dd3690529fa866a3fba5b76810b51f3e007
Ngoc <nhngoc1506@gmail.com>
2022-03-26 22:00:09 +0700

    edit file b2.cpp and delete printf

--- b2.cpp
+++ b2.cpp
@@ -6,11 +6,7 @@ int a1,b1,c1,a2,b2,c2;
 float D,Dx,Dy,x,y;
 printf("a1*x + b1*y = c1\n");
 printf("a2*x + b2*y = c2\n");
-printf("\nNhap a1: "); scanf("%d", &a1);
-printf("\nNhap b1: "); scanf("%d", &b1);
-printf("\nNhap c1: "); scanf("%d", &c1);
-printf("\nNhap a2: "); scanf("%d", &a2);
-printf("\nNhap b2: "); scanf("%d", &b2);
+
 printf("\nNhap c2: "); scanf("%d", &c2);
 D = a1 * b2 - a2 * b1;
 Dx = c1 * b2 - c2 * b1;
